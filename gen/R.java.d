6f2b63d25e31e0ee314c0a53c19ff0a879ecfea0
caidongyu <dongyu.cai@imduoduo.com>
2013-04-17 18:03:13 +0800

    import poject

---
+++ gen/R.java.d
@@ -0,0 +1,4 @@
+C:\Users\sky.wing\Desktop\HelloWorld\gen\com\helloworld\R.java \
+ : C:\Users\sky.wing\Desktop\HelloWorld\res\layout\main.xml \
+C:\Users\sky.wing\Desktop\HelloWorld\res\values\strings.xml \
+C:\Users\sky.wing\Desktop\HelloWorld\bin\AndroidManifest.xml \
